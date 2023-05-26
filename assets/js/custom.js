4542a3f10c4ce1466c316bf34efde37ce36c2d8e
NAVSKIE <ronnel.navarro2020@gmail.com>
2023-05-26 23:51:41 +0800

    animation

---
+++ assets/js/custom.js
@@ -0,0 +1,21 @@
+const faders = document.querySelectorAll('.fade-in');
+
+const appearOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px 50px 0px",
+};
+
+ const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+    entries.forEach(enrty => {
+        if (!enrty.isIntersecting) {
+            return;
+        } else {
+            enrty.target.classList.add('appear');
+            appearOnScroll.unobserve(enrty.target);
+        }
+    })
+ }, appearOptions);
+
+ faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+ })
